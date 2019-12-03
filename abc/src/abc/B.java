16a0f5aa3375b9f9227cd55b0155e28e9c8cd4b5
tedu <tedu@DESKTOP-5FTQJJE>
2019-12-03 11:07:26 +0800

    本地

--- abc/src/abc/B.java
+++ abc/src/abc/B.java
@@ -3,6 +3,7 @@ package abc;
 public class B {
            public static void main(String[] args) {
            System.out.println("你爸死了"); 
+           System.out.println("你爷爷先死");
            }
 
 }
